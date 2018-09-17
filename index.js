e3ec05f8f70585d06210f9d48db33ce5d7e1c5fc
Eugenio Gonzalez <e.gzzart@gmail.com>
2018-09-17 17:50:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 function shout(string){
   return string.toUpperCase()
 }
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  switch (string) {
+    case string === "I love you, Garndma.":
+        return "I love you too."
+      break;
+    case string === string.toLowerCase()
+        return "I can't hear you!"
+      break;
+    case string === string.toUpperCase():
+        return "YES INDEED"
+      break;
+    default:
+      
+  }
+}
